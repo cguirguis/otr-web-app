fad51036a9f34d58acb65628fb5f87d6e17359b7
Chris Guirguis <cguirguis@gmail.com>
2016-02-12 04:24:51 -0800

    Configure for prod

--- www/js/config/Config.js
+++ www/js/config/Config.js
@@ -9,6 +9,12 @@ WebApp.constant("Constants", {
     'stripeClientId': 'pk_test_fHIOKc7Sf7gNjwUIIT3XJfDt',
     'debug': true
   },
+  PROD: {
+    'name': 'DEVO',
+    'apiEndpoint': 'https://otr-backend-service-us-devo.offtherecord.com/api/v1/',
+    'stripeClientId': 'pk_test_fHIOKc7Sf7gNjwUIIT3XJfDt',
+    'debug': true
+  },
   CONFIGS: {
     'DaysToRespondToTicket' : 15
   },
